13cb31a31abfccfda1196e149103c0a34a964088
Aidan Liam N. Battad <142377920+AidanBattad@users.noreply.github.com>
2023-10-24 23:30:52 +0800

    Create RefrigeratorAdapter.java

---
+++ RefrigeratorAdapter.java
@@ -0,0 +1,14 @@
+package adapterPattern;
+
+class RefrigeratorAdapter implements DeviceAdapter {
+    private Refrigerator refrigerator;
+    public RefrigeratorAdapter(Refrigerator refrigerator)
+    {
+        this.refrigerator = refrigerator;
+    }
+
+    public void plugIn()
+    {
+        refrigerator.startCooling();
+    }
+}
